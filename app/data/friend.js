a7872341ee68ff5a6762ffb2a5ca0c430a6ee856
priscila <deavila.priscila@outlook.com>
2018-03-06 16:04:02 -0800

    point

---
+++ app/data/friend.js
@@ -0,0 +1,100 @@
+// ===============================================================================
+// This data will hold all of the friends.
+// ===============================================================================
+
+
+var friendsData = [
+
+    {
+
+        "name": "Malcolm Young",
+
+        "photo": "../public/images/malcolmyoung.jpg",
+
+        "scores": [10, 10, 10, 10, 10, 12, 12, 12, 12, 12]
+
+    },
+
+
+    {
+
+        "name": "Dave Evans",
+
+        "photo": "../public/images/daveevans.jpg",
+
+        "scores": [25, 20, 15, 10, 12, 25, 20, 15, 10, 20]
+
+    },
+
+
+    {
+
+        "name": "Larry Van Kriedt",
+
+        "photo": "../public/images/larry-van-kriedt.jpg",
+
+        "scores": [5, 10, 15, 20, 25, 5, 10, 11, 1, 12]
+
+    },
+
+
+    {
+
+        "name": "Bon Scott",
+
+        "photo": "../public/images/bonscott.jpg",
+
+        "scores": [15, 11, 15, 10, 10, 20, 25, 30, 20, 12]
+
+    },
+
+
+    {
+
+        "name": "Phil Rudd",
+
+        "photo": "../public/images/philrudd.jpg",
+
+        "scores": [50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
+
+    },
+
+
+    {
+
+        "name": "Cliff Williams",
+
+        "photo": "../public/images/cliffwilliams.jpg",
+
+        "scores": [90, 90, 90, 90, 90, 90, 90, 90, 90, 90]
+
+    },
+
+
+    {
+
+        "name": "Brian Johnson",
+
+        "photo": "../public/images/brainjohnson.jpg",
+
+        "scores": [20, 25, 30, 50, 25, 35, 40, 15, 1, 1]
+
+    },
+    
+
+    {
+
+        "name": "Simon Wright",
+
+        "photo": "../public/images/simonwright.jpg",
+
+        "scores": [25, 25, 25, 25, 25, 25, 25, 25, 25, 25]
+
+    }
+
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+
+module.exports = friendsData;
---
+++ server.js
@@ -0,0 +1,36 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/app')); 
+
+
+// ROUTER
+
+// The below points our server to a series of "route" files.
+
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+require("./app/routing/apiRoutes")(app);
+
+require("./app/routing/htmlRoutes")(app);
+
+
+
+// Starts the server to begin listening
+
+app.listen(PORT, function() {
+
+  console.log("Listing to this port" + PORT);
+});
